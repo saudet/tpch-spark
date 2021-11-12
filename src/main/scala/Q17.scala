2f9fb7a74eb0614b3c9222e2a7f5c5c3f541bba8
Samuel Audet <samuel.audet@gmail.com>
2021-11-12 22:33:30 +0900

    Modify Q17 to use Frovedis

--- src/main/scala/Q17.scala
+++ src/main/scala/Q17.scala
@@ -1,10 +1,14 @@
 package main.scala
 
+import com.nec.frovedis.sql.FrovedisDataFrame
+import com.nec.frovedis.sql.functions.avg
+import com.nec.frovedis.sql.functions.sum
+import com.nec.frovedis.sql.implicits_._
 import org.apache.spark.sql.DataFrame
 import org.apache.spark.SparkContext
-import org.apache.spark.sql.functions.avg
-import org.apache.spark.sql.functions.sum
-import org.apache.spark.sql.functions.udf
+//import org.apache.spark.sql.functions.avg
+//import org.apache.spark.sql.functions.sum
+//import org.apache.spark.sql.functions.udf
 
 /**
  * TPC-H Query 17
@@ -16,25 +20,43 @@ class Q17 extends TpchQuery {
   override def execute(sc: SparkContext, schemaProvider: TpchSchemaProvider): DataFrame = {
 
     // this is used to implicitly convert an RDD to a DataFrame.
-    val sqlContext = new org.apache.spark.sql.SQLContext(sc)
-    import sqlContext.implicits._
+//    val sqlContext = new org.apache.spark.sql.SQLContext(sc)
+//    import sqlContext.implicits._
     import schemaProvider._
 
-    val mul02 = udf { (x: Double) => x * 0.2 }
+    val time0 = System.nanoTime()
 
-    val flineitem = lineitem.select($"l_partkey", $"l_quantity", $"l_extendedprice")
+    val flineitem = new FrovedisDataFrame(lineitem)
+    val fpart = new FrovedisDataFrame(part)
 
-    val fpart = part.filter($"p_brand" === "Brand#23" && $"p_container" === "MED BOX")
-      .select($"p_partkey")
-      .join(lineitem, $"p_partkey" === lineitem("l_partkey"), "left_outer")
+//    val mul02 = udf { (x: Double) => x * 0.2 }
+
+    val time1 = System.nanoTime()
+
+//    val flineitem2 = flineitem.select($$"l_partkey", $$"l_quantity", $$"l_extendedprice")
+
+    val fpart2 = fpart.filter($$"p_brand" === "Brand#23" && $$"p_container" === "MED BOX")
+      .select($$"p_partkey")
+      .join(flineitem, $$"p_partkey" === flineitem("l_partkey"), "leftouter")
+    val fpart3 = fpart2.withColumnRenamed("p_partkey", "key")
     // select
 
-    fpart.groupBy("p_partkey")
-      .agg(mul02(avg($"l_quantity")).as("avg_quantity"))
-      .select($"p_partkey".as("key"), $"avg_quantity")
-      .join(fpart, $"key" === fpart("p_partkey"))
-      .filter($"l_quantity" < $"avg_quantity")
-      .agg(sum($"l_extendedprice") / 7.0)
+    val ret = fpart2.groupBy("p_partkey")
+      .agg(avg($$"l_quantity").as("avg_quantity"))
+      .select($$"p_partkey", $$"avg_quantity")
+      .join(fpart3, $$"p_partkey" === fpart3("key"))
+      .filter($$"l_quantity" < $$"avg_quantity")
+      .agg(sum($$"l_extendedprice"))
+
+    val time2 = System.nanoTime()
+
+    ret.count
+
+    val time3 = System.nanoTime()
+
+    System.out.println("TIMES = " + (time1 - time0) / 1000000 + " " + (time2 - time1) / 1000000 + " " + (time3 - time2) / 1000000)
+
+    ret
   }
 
 }
